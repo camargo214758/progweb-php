a7c422a194dbf4039b2c8bbefe3367aa8d4cd0d4
camargo214758 <guisafeju@gmail.com>
2022-04-27 16:56:17 -0300

    Create exercicio03.php

---
+++ exercicio03.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valor de Salario</title>
+
+</head>
+<body>
+    <h1>condicionais</h1>
+    <hr>
+    <h2>Calculo</h2>
+    <?php
+    $salario = 500; 
+
+    if( $salario < 500){
+       $rejuste = $salario*1.15;
+    }
+    
+    ?>
+
+    
+</body>
+</html>
