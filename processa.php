0a1472d9a35a5c537ea7ce7f928e29af1ae63f1e
camargo214758 <guisafeju@gmail.com>
2022-05-05 16:35:50 -0300

    formulario

--- processa.php
+++ processa.php
@@ -10,7 +10,19 @@
     <h1> Procesamento de Dados</h1>
     <hr>
 
-    <?php    
+    <?php
+    if( empty( $_POST["nome"]) || empty($_POST["email"]) ){
+        ?>
+
+        <p style="color:red">Por favor preencha nome e e-mail!</p>
+        <p> <a href="08-formulario.html">Voltar</a></p> <!--opcional-->
+        <?php
+        echo "<p>Por favor preencha nome e e-mail!<p>";
+       
+
+    } else {
+    
+    
     /*echo "<pre>";
     var_dump($_POST);
     echo "</pre"; */
@@ -28,6 +40,7 @@
     ?>
     <h2>Dados</h2>
     <ul>
+    <?php } ?>
 
     <li> Nome: <?=$nome?> </li>
     <li> E-mail: <?=$email?> </li>
@@ -44,11 +57,12 @@
         </ul>
 
 </li>
-<?php } ?>
+
     <li> Informativos: <?=$informativos?> </li>
     <li> Mensagem :<?=$mensagem?> </li>
 
     </ul>
+    <?php } ?>
     
 </body>
 </html>
